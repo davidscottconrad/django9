95e0c84771e940180dd184ef34fea7060541224c
davidscottconrad <david.scott.conrad@gmail.com>
2024-02-17 21:19:20 -0500

    temp 404 page

--- ninthdjango/settings.py
+++ ninthdjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7tebto2_*=9k&@l3ctz1#hfrf(zqi&r)n-+ujd41sm**c3f2*)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".awsapprunner.com", "127.0.0.1"]
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'ninthdjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +68,7 @@ TEMPLATES = [
             ],
         },
     },
+
 ]
 
 WSGI_APPLICATION = 'ninthdjango.wsgi.application'
