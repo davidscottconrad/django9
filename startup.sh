1fa3204a07d327f0a97b83f2d7b096ca9ff9f59a
davidscottconrad <david.scott.conrad@gmail.com>
2024-02-24 23:05:17 -0500

    startup script for cors header

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 ninthdjango.wsgi
+pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 ninthdjango.wsgi
