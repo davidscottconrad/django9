3368c09b071e7b0cf806dbd0760a57aa83bc4b8d
davidscottconrad <david.scott.conrad@gmail.com>
2024-02-24 23:11:05 -0500

    remove that bit

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 ninthdjango.wsgi
+python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 ninthdjango.wsgi
