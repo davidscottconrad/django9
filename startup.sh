4f1e166d388aad48a1220d073a4398d9f349a51c
davidscottconrad <david.scott.conrad@gmail.com>
2024-03-11 13:39:13 -0400

    port binding issue?

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py migrate && python manage.py collectstatic --noinput && gunicorn --workers 2 ninthdjango.wsgi
+python manage.py migrate && python manage.py collectstatic --noinput && gunicorn ninthdjango.wsgi --bind 0.0.0.0:8000 --workers 2
